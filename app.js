5f7f36a42b558b04815c58ce4b5932de3059c403
Dayna <daynajblackwell@gmail.com>
2018-10-02 19:15:22 -0700

    Changed error messages returnd by server

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const mongo = require('mongodb');
 const mongoose = require('mongoose');
 const log = msg => console.log(msg);
 const app = express();
+const port = process.env.port || 3000;
 
 shortid.characters('0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ$@');
 app.use(cors());
@@ -21,40 +22,46 @@ app.get('/', (req, res)=>{
 });
 
 app.post("/new/:url(*)", (req,res) => {
-  log(`url POSTED is ${req.body.url}`);
   let url = req.body.url;
+  log(`url is ${url}`);
+
   
   if(validUrl.isWebUri(url)) {
     // the URL is valid
     log('url is syntactically valid..');
     
     dns.lookup(url.split('//')[1].split('/')[0], (err, addresses, family) => {
-    if(err) return log(err); // dns failed
+    if(err) return log(err); 
     else {
       // At this point, the URL has been syntax validated and dns validated.  
       console.log(`url: ${url} passes dns lookup`);
-
+      // database stuff here...
       mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true }, (err, db) => {
       if(err) throw err;
       else {
         log(`Connection to ${mongoose.connection.name} database established`);
+        // breaks here!!!!!!!!!!!!!!!!!!!!!!!
+        
         // check to see whether url is already in the database
         let query = {originalUrl: url};
         db.collection("urls").findOne(query, (err,result)=> {
           if(err) throw err;
           if(result != null){
             log(`url already in database: ${result.originalUrl}`);
-            log(`Shortened url will be:${req.hostname}/${result.shortCode}`);
+            // res.redirect(result.originalUrl);
+            // res.end('redirect should occur here');
+            log(`${req.hostname}/${result.shortCode}`);
             res.send({url:`${req.hostname}/${result.shortCode}`});
           } 
           else {
             // result was NOT FOUND IN DATABASE
+            // res.redirect("/");
             log('result not found in database');
+            // need to add result to database
             let urlObj = {
               shortCode: shortid.generate(),
               originalUrl: url,
             };
-            // need to add result to database
             db.collection("urls").insertOne(urlObj, (err,data) => {
               if(err) throw err;
               console.log("1 document inserted");
@@ -66,17 +73,17 @@ app.post("/new/:url(*)", (req,res) => {
           }
         });
       }
-      })
+      }).catch(err=>log(err));
     }});
   } else { 
     log('url is syntactically invalid..');
-    res.end('url is syntactically invalid..');
+    res.end("err");
   }
+  
 })
 
-app.get('/:shortCode(*)', (req,res)=>{
+app.get('/:shortCode(*)', (req,res) => {
   // redirect requests to the original url from database
-  // query database by shortCode route parameters
   let shortCode = req.params.shortCode;
   mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true }, (err, db) => {
     if(err) throw err
@@ -90,11 +97,9 @@ app.get('/:shortCode(*)', (req,res)=>{
         res.redirect(result.originalUrl);
         res.end(`${shortCode}`)
       } else {
-         res.redirect('/');
-         res.end('shortCode not found in database');
+          res.end('shortCode not found in database');
       }
     });
   });
 });
-let port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`server started on port ${port}`));
